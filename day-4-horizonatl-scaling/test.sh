06e0860517ce11bba60ac7e9df52198c19a6e8e3
root <root@ip-172-31-92-51.ec2.internal>
2026-02-22 03:49:04 +0000

    new

--- day-4-horizonatl-scaling/test.sh
+++ day-4-horizonatl-scaling/test.sh
@@ -1,6 +1,6 @@
 for j in {1..10}; do  
     for i in {1..1000}; do  
-        curl -s -o /dev/null -w "%{http_code}\n" http://a5519ee8ee62147a7b7c055673f1ee70-2108986447.us-east-1.elb.amazonaws.com/ &  
+        curl -s -o /dev/null -w "%{http_code}\n" http://aecf329fcf96043d78fe56d98f1e8278-1149846043.us-east-1.elb.amazonaws.com/ &  
     done  
     wait  # Wait for all background curl processes to finish before next iteration
 done
